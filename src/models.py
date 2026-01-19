03b78045bb3abc5917e64dc133fec398986f6d06
ricardo.schoettler <ricardo.schoettler@gmail.com>
2026-01-20 00:23:26 +0100

    dev models.py

--- src/models.py
+++ src/models.py
@@ -16,13 +16,15 @@ class ReviewState(BaseModel):
     date_created: int = Field(..., description="The date the review was created in epoch time")
     date_modified: int = Field(..., description="The date the review was last modified")
     reviews: list[dict] = Field(default_factory=list, description="The list of reviews")
+    reviewed_ids: list[str] = Field(default_factory=list, description="The list of reviewed document ids")
 
 
 class Review(BaseModel):
     id: int = Field(..., description="The unique identifier for the review")
     user_id: int = Field(..., description="The unique identifier for the user who made the review")
     collection_ids: list[str] = Field(default_factory=list, description="The list of collection ids")
-    review_state: ReviewState = Field(..., description="The state of the review")
+    state: ReviewState = Field(..., description="The current state of the review")
+    reviewd_states: list[ReviewState] = Field(default_factory=list, description="The list of past reviewd states")
 
 
 
