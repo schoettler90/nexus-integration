9459dc444da02531e4317716cab88bafac84aa49
ricardo.schoettler <ricardo.schoettler@gmail.com>
2026-01-20 00:10:06 +0100

    add user and review models with Pydantic

--- src/models.py
+++ src/models.py
@@ -1,2 +1,28 @@
 from pydantic import BaseModel, Field
+from typing import Optional
+
+class User(BaseModel):
+    id: str = Field(..., description="The unique identifier for the user")
+    name: str = Field(..., description="The name of the user")
+
+class Collection(BaseModel):
+    id: str = Field(..., description="The unique identifier for the collection")
+    collection_name: str = Field(..., description="The name of the collection")
+    document_ids: list[str] = Field(default_factory=list, description="The list of document ids")
+
+
+class ReviewState(BaseModel):
+    review_id: str = Field(..., description="The unique identifier for the review")
+    date_created: int = Field(..., description="The date the review was created in epoch time")
+    date_modified: int = Field(..., description="The date the review was last modified")
+    reviews: list[dict] = Field(default_factory=list, description="The list of reviews")
+
+
+class Review(BaseModel):
+    id: int = Field(..., description="The unique identifier for the review")
+    user_id: int = Field(..., description="The unique identifier for the user who made the review")
+    collection_ids: list[str] = Field(default_factory=list, description="The list of collection ids")
+    review_state: ReviewState = Field(..., description="The state of the review")
+
+
 
